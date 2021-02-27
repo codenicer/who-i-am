4e3a7a2eb1fe8159e1e864c48881be434ed8d530
codenicer <ruthertenido@gmail.com>
2021-02-27 17:14:06 +0800

    fixing about me

--- pages/main.js
+++ pages/main.js
@@ -6,6 +6,7 @@ import Qoute from '../componets/Qoute'
 import MainPageCard from '../componets/MainPageCard'
 import { doneLoadingState } from '../provider/DoneLoadingContext'
 import { techList } from '../utils/tech-list'
+import { about } from '../utils/about'
 
 export default function Main() {
   const doneState = doneLoadingState()
@@ -21,22 +22,10 @@ export default function Main() {
               <h1 className={styles.about_title}>About</h1>
               <div className={styles.vertical_line}></div>
             </div>
-            <p className={styles.about_subject}>
-              In pharetra, massa porta pharetra posuere, tellus diam scelerisque
-              purus, ut elementum sem diam in nisl. Proin at quam volutpat,
-              bibendum dolor vitae, iaculis quam. Integer quis gravida odio,
-              vitae aliquam tellus.{' '}
-            </p>
-            <p className={styles.about_subject}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dui
-              odio, tincidunt et erat eget, ultricies tempor turpis. Vivamus
-              eget pellentesque nunc, sed maximus orci. Etiam gravida vitae
-              justo et congue. Suspendisse sit amet vestibulum leo. Nunc
-              facilisis augue non justo congue, sed viverra tortor laoreet.{' '}
-            </p>
-            <p className={styles.about_subject}>
-              Here`s the technologies Ive been enhancing through my works:
-            </p>
+            <text className={styles.about_subject}>{about.nameTitle}</text>
+            <text className={styles.about_subject}>{about.firstP}</text>
+            <p className={styles.about_subject}>{about.secondP}</p>
+            <p className={styles.about_subject}>{about.techAbout}</p>
             <ul className={styles.tech_list}>
               {techList.map((tech, i) => {
                 return <li key={i}>{tech}</li>
---
+++ utils/about.js
@@ -0,0 +1,9 @@
+export const about = {
+  nameTitle: 'Hi ! Ruther here!',
+  firstP:
+    'I am a Full-stack Web Developer in the Philippines, a programming enthusiast. I love spending time working with different aspects of web developing for it is like a never-ending learning,Everyday comes a new discovery...',
+  secondP:
+    ' I love working with logic and structures of coding , whether it be simple or wide variety of projects. ',
+  techAbout:
+    'Here are few of the programming languages & tools that I have been working on with my projects:',
+}
