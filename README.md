e4249a1562171741710b99ba04aef76e31fe7412
codenicer <ruthertenido@gmail.com>
2021-03-01 16:10:45 +0000

    Deploying to gh-pages from  @ 2326fdb6fbb19018ae5dd6a6d7ed0948cd324c60 🚀

--- README.md
+++
@@ -1,1 +0,0 @@
-Please dont forget to have credits on me when using this project, Thank you. Also check brittanychiang.com , She`s a great developer.
--- utils/general.js
+++
@@ -1,7 +0,0 @@
-export const generalSetting = {
-  emailTo: 'mailto:ruthertenido@gmail.com',
-  github: 'https://github.com/codenicer',
-  facebook: 'https://www.facebook.com/code.Nicer/',
-  linkedin: 'https://www.linkedin.com/in/code-nicer/',
-  twitter: '#',
-}
