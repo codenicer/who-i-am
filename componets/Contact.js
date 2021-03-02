7de1f8c7389885d662a54b0eabbf2a2449c4cfb5
codenicer <ruthertenido@gmail.com>
2021-03-03 04:57:54 +0800

    update misspelled words LOL

--- componets/Contact.js
+++ componets/Contact.js
@@ -11,9 +11,9 @@ export default function Contact() {
           <h1 className={styles.contat_title}>Get In Touch</h1>
           <p className={styles.contact_message}>
             I do enjoy freelancing, but I`m currently looking for any new
-            opportunities, my inbox is always open. Whether you have an
-            interesnt working with me or have a questions, Im happy to respond
-            to you. God Speed!
+            opportunities, my inbox is always open. Whether you have an interest
+            working with me or have a questions, Im happy to respond to you. God
+            Speed!
           </p>
           <a href={generalSetting.emailTo} className={styles.contact_btn}>
             <button>Email Me</button>
