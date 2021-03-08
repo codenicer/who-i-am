069f47cdb3783f2f55e5280b0b9a7e9236831e6c
codenicer <ruthertenido@gmail.com>
2021-03-08 21:44:41 +0800

    correct missedspell

--- componets/Contact.js
+++ componets/Contact.js
@@ -11,7 +11,7 @@ export default function Contact() {
           <h1 className={styles.contat_title}>Get In Touch</h1>
           <p className={styles.contact_message}>
             I do enjoy freelancing but, I am currently looking for any
-            oportunities, my inbox is always open. Whether you have an interest
+            opportunities, my inbox is always open. Whether you have an interest
             working with me or have any questions, I will be pleased to respond.
             God Speed!
           </p>
