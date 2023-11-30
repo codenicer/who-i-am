95e52b18a819a0e55e09f88fa80634883e788337
Tenido, Ruther <ruthertenido@gmail.com>
2023-11-30 20:19:36 +0800

    Update about.js
    Udate about

--- utils/about.js
+++ utils/about.js
@@ -1,9 +1,9 @@
 export const about = {
   nameTitle: 'Hi ! Ruther here!',
   firstP:
-    'I am a Full-stack Web Developer in the Philippines, a programming enthusiast. I love spending time working with different aspects of web developing for it is like a never-ending learning, Everyday comes a new discovery...',
+    'I\'m a dedicated Full-stack Web Developer in the Philippines and a programming enthusiast. Embracing the ever-evolving nature of web development, I find joy in continuous learning. Each day brings new discoveries...',
   secondP:
-    ' I love working with logic and structures of coding , whether it be simple or wide variety of projects. ',
+    'I love working with logic and structures of coding, whether it be simple or a wide variety of projects. ',
   techAbout:
-    'Here are few of the programming languages & tools that I have been working on with my projects:',
+    'Here are a few of the programming languages & tools that I have been working on with my projects:',
 }
