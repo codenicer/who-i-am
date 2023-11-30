5544c6249c811d066afd4eaf946d9245f1c0c457
Tenido, Ruther <ruthertenido@gmail.com>
2023-11-30 20:14:18 +0800

    Update general.js
    update main page message

--- utils/general.js
+++ utils/general.js
@@ -7,5 +7,5 @@ export const generalSetting = {
   maxProjectRender: 3,
   quoute: '“Everything is difficult until you know how to do it.”',
   maingPageMessage:
-    'I am a Full Stack Web Developer based in the Philippines. Building a web application is what I do. From constructing through deployment. Continuous integration and development for the application is a must.',
+    'I am a dedicated Full Stack Web Developer located in the Philippines, specializing in the end-to-end development of dynamic web applications. My expertise encompasses the entire development lifecycle, from initial concept and design to seamless deployment. I prioritize the implementation of continuous integration and development practices to ensure the ongoing enhancement and refinement of applications, delivering optimal user experiences. Passionate about leveraging innovative technologies to create robust and scalable web solutions, I am committed to crafting high-quality, performance-driven applications that meet the evolving needs of clients and users.',
 }
